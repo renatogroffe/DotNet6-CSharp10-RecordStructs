b833a4a36d46c8f09220ae7192a1e9fb73dc5342
Renato Groffe <renato.groffe@yahoo.com.br>
2022-01-22 17:18:25 -0300

    .NET 6 + Console Application + C# 10 + Record Structs
    .NET 6 + Console Application + C# 10 + Record Structs

---
+++ ExemploRecordStructs/RecordStructs.cs
@@ -0,0 +1,18 @@
+namespace ExemploRecordStructs;
+
+public record struct Demografia
+{
+    public string Pais { get; init; }
+    public int Ano { get; set; }
+    public int Populacao { get; set; }
+
+    public Demografia()
+    {
+        Pais = "Brasil";
+        Ano = 2000;
+        Populacao = 174_800_000;
+    }
+    
+    public Demografia(string pais, int ano, int populacao) =>
+        (Pais, Ano, Populacao) = (pais, ano, populacao);
+}
